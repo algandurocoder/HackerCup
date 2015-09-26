eca65e12b2d53c681a984b10deaf22ec7f6e99a3
Alganduro Coder <algandurocoding@gmail.com>
2015-09-26 22:21:45 +0100

    Initial commit

---
+++ BeautifulString/BeautifulString.Engine/StringBeautyCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulString.Engine
+{
+    public static class StringBeautyCalculator
+    {
+        public static Int32 CalculateStringBeauty(string inputString)
+        {
+            //Ignore case
+            inputString = inputString.ToLower();
+
+            //initialize alphabet dicionary
+            Dictionary<char, int> d = initializeDictionary();
+
+            //count the frequency of each letter of the string
+            foreach (char c in inputString)
+                if (char.IsLetter(c))
+                    d[c] += 1;
+
+
+            Int32 currentValue = 26;
+            Int32 beauty = 0;
+
+            foreach (var item in d.OrderByDescending(c => c.Value))
+            {
+                beauty += (item.Value * currentValue);
+                currentValue--;
+            }
+
+            return beauty;
+        }
+
+        private static Dictionary<char, int> initializeDictionary()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>(26);
+
+            for (char i = 'a'; i <= 'z'; i++)
+                d.Add(i, 0);
+
+            return d;
+        }
+
+    }
+}
---
+++ BeautifulString/BeautifulString.EngineTests/BeautifulStringCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeautifulString.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulString.Engine.Tests
+{
+    [TestClass()]
+    public class BeautifulStringCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateStringBeautyTest()
+        {
+            string string1 = "ABbCcc";
+            string string2 = "Good luck in the Facebook Hacker Cup this year!";
+            string string3 = "Ignore punctuation, please :)";
+            string string4 = "Sometimes test cases are hard to make up.";
+            string string5 = "So I just go consult Professor Dalves";
+
+            Assert.AreEqual(152, StringBeautyCalculator.CalculateStringBeauty(string1));
+            Assert.AreEqual(754, StringBeautyCalculator.CalculateStringBeauty(string2));
+            Assert.AreEqual(491, StringBeautyCalculator.CalculateStringBeauty(string3));
+            Assert.AreEqual(729, StringBeautyCalculator.CalculateStringBeauty(string4));
+            Assert.AreEqual(646, StringBeautyCalculator.CalculateStringBeauty(string5));
+        }
+    }
+}
