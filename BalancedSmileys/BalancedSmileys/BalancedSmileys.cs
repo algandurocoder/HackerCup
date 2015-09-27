74b707f200cae7eacf35e52dc70a2dc52bebf388
Alganduro Coder <algandurocoding@gmail.com>
2015-09-27 22:12:48 +0100

    initial version

---
+++ BalancedSmileys/BalancedSmileys/BalancedSmileys.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedSmileys
+{
+    public class BalancedSmileys
+    {
+        public static Boolean AreSmileysBalanced(string inputString)
+        {
+            
+            Int32 openParenthesis = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '(')
+                {
+                    if (i == 0 || inputString[i - 1] != ':')
+                        openParenthesis++;
+                }
+                else if (inputString[i] == ')')
+                {        
+                    if (i == 0 || inputString[i - 1] != ':')
+                        openParenthesis--;
+                    if(openParenthesis < 0)
+                        break;
+                }
+            }
+
+            return openParenthesis == 0;
+                
+        }
+
+    }
+}
---
+++ BalancedSmileys/BalancedSmileys/aBalancedSmileys.tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BalancedSmileys
+{
+    [TestFixture]
+    class BalancedSmileysTests
+    {
+        [TestCase("", true)]
+        [TestCase("sad sd :", true)]
+        [TestCase("(I am balanced)", true)]
+        [TestCase("(I am balanced)(I am too!)", true)]
+        [TestCase(":)", true)]
+        [TestCase(":(", true)]
+        [TestCase("(", false)]
+        [TestCase("()", true)]
+        [TestCase(":(:)", true)]
+        [TestCase("(:)", false)]
+        [TestCase("::(:)", true)]
+        [TestCase("(:))", true)]
+        [TestCase("(())", true)]
+        [TestCase("()()", true)]
+        [TestCase(")(", false)]
+        [TestCase("(:):):(:::)", false)]
+        public void TestBalance(string inputString, Boolean expectedResult)
+        {
+            Boolean result = BalancedSmileys.AreSmileysBalanced(inputString);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
